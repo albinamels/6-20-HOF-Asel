381a3c5eb0107537566c8e8b91d3ba31846aecb4
albinamels <albina.mels@gmail.com>
2023-06-26 22:28:24 -0500

    local storage, toggle mode

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const searchForm = document.querySelector('#search-form');
 const celciusBtn = document.querySelector('.celcius')
 const fahrenheitBtn = document.querySelector('.fahrenheit');
 const body = document.querySelector('body');
+const saveLocBtn = document.querySelector('.save-location')
+const modeBtn = document.querySelector('.mode')
+
 
 // 4 step
 let unit = 'imperial'; // Fahrenheit
@@ -24,24 +27,38 @@ fahrenheitBtn.addEventListener('click', () => {
   celciusBtn.classList.remove('active');
 })
 
-// 1 step
+// 8 step - setItem('', )
+saveLocBtn.addEventListener('click', () => {
+  const city = cityInput.value;
+  localStorage.setItem('cityName', city);
+  alert('Your city is successfully saved')
+})
+
+// 9 step - getItem()
+document.addEventListener('DOMContentLoaded', () => {
+  const storedCity = localStorage.getItem('cityName');
+  fetchWeatherData(storedCity);
+  fetchPhotos(storedCity);
+})
+
+// 1, 9 step
 searchForm.addEventListener('submit', (event) => {
   event.preventDefault(); // prevent default behavior of form
-  fetchWeatherData();
-  fetchPhotos();
+  fetchWeatherData(cityInput.value);
+  fetchPhotos(cityInput.value);
 });
 
-// 2 step
-const fetchWeatherData = async() => {
+// 2, 9 step
+const fetchWeatherData = async(city) => {
   //const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=warsaw&appid=d030ac84773f6c1da5dcb6a7fe964373`);
-  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=${unit}&appid=d030ac84773f6c1da5dcb6a7fe964373`);
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=d030ac84773f6c1da5dcb6a7fe964373`);
   const data = await res.json();
   displayData(data);  
 };
 
-// 6 step
-const fetchPhotos = async () => {
-  const res = await fetch(`https://api.unsplash.com/search/photos?query=${cityInput.value}&client_id=K1z7daCOfTevvq6JyTpQCEB3wXsdfC4N1d4H4WPWtxM`);
+// 6, 9 step
+const fetchPhotos = async(city) => {
+  const res = await fetch(`https://api.unsplash.com/search/photos?query=${city}&client_id=K1z7daCOfTevvq6JyTpQCEB3wXsdfC4N1d4H4WPWtxM`);
   const data = await res.json();
   const imageUrl = data.results[3].urls.full;
   renderBackground(imageUrl);
@@ -65,3 +82,19 @@ const displayData = (data) => {
   //unit = 'metric' ? temp.innerText = `${Math.round(data.main.temp)}° C` : temp.innerText = `${Math.round(data.main.temp)}° F`;  
 }
 
+modeBtn.addEventListener('click', () => {
+  if(modeBtn.innerText === 'Dark Mode'){
+    modeBtn.innerText = 'Light Mode'; 
+    body.className = 'dark';
+  } else {
+    modeBtn.innerText = 'Dark Mode';  
+    body.className = 'light'; 
+  }
+})
+
+//task:
+// 1. create a button called "Dark Mode"
+// 2. as i click on the button, I want my screen to be dark (either totally black or darker shade of the background picture)
+// 3. if i click on the button again, I want my screen to go back to normal light mode.
+// extra: make button name dynamic, toggle its name between dark mode and light mode
+// extra extra: save dark mode preferences in the local storage.
